ef61cad5fdcd7f5cbaa90de564680356e8576df8
sumitpandit <sumitpandit178@gmail.com>
2020-10-01 13:32:33 +0530

    [Sumit] Add: Create_Contact_in_AddressBook

---
+++ AddressBook/AddressBook/AddressBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+		private LinkedList<ContactDetails> addBook;
+        public AddressBook()
+        {
+			this.addBook = new LinkedList<ContactDetails>();
+        }
+		public void setAddressBookBuilder(LinkedList<ContactDetails> contact)
+		{
+			this.addBook = contact;
+		}
+		public LinkedList<ContactDetails> getAddressBook()
+		{
+			return this.addBook;
+		}
+		public void addContact(ContactDetails contactObj)
+		{
+			addBook.AddLast(contactObj);
+		}
+		public LinkedList<ContactDetails> showContact()
+		{
+			return addBook;
+		}
+	}
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welome to Address Book!!!");
+            Console.WriteLine("Add contact details here");
+        }
+    }
+}
